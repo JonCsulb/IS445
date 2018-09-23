b0707ad4d7b06c973d7ef0aa634ad4f58514e4b4
Jon <jonngu95@gmail.com>
2018-09-23 13:10:05 -0700

    IsHw2 Barebones

---
+++ JS Folder/t1.js
@@ -0,0 +1,17 @@
+const myButton = document.getElementById('t2-button')
+
+
+
+
+
+function onClick ()
+        console.log('before alert')
+        {alert('Hello world after click')
+        console.log('before alert')
+    }
+
+     
+
+
+
+myButton.addEventListener('click', onClick)
