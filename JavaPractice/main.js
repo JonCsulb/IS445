48e36fc8656ae49e8133f413b9890c17c4693d8f
Jon <jonngu95@gmail.com>
2018-09-27 13:31:35 -0700

    New Folder and files for Ninja

---
+++ JavaPractice/main.js
@@ -0,0 +1,11 @@
+const btn = document.getElementById('button');
+
+const rainbow = [
+        'red', 'oragne','yellow','green','blue','purple','violet'
+            ];
+
+function change() {
+        document.body.style.background = rainbow[Math.floor(7*Math.random())];
+                }
+
+btn.addEventListener('click',change);
