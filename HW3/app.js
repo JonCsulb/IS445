2b197c2bb1bb399f3c7dece9913ad61f86d0fbaa
Jon <jonngu95@gmail.com>
2018-10-03 10:05:29 -0700

    Working Array Function, Sum Function Left

--- HW3/app.js
+++ HW3/app.js
@@ -1,29 +1,74 @@
 const Start = prompt('Please give Start Number:')
 const End   = prompt('Please give End Number:')
 const Step  = prompt('Please give Step Number:')
-
-// Convert and  Check input for non integers
+const NumArray = [] 
+// Convert input to number
 const StartNumber = Number(Start);
 const EndNumber   = Number(End);
 const StepNumber  = Number(Step);
-let result = "Result"
+
+// result = ResultArray +  '<br>' + ResultSum + '<br>' + ResultBin
+
+
+
+
+
 
 if ((Number.isInteger(StartNumber+EndNumber+StepNumber) === false)
      || StepNumber == 0)
     {
-        result = ("Invalid integer input or step number is 0, please reload and try again") 
+        let ResultError = ("Invalid integer input or step number is 0, please reload and try again") 
     }
 else
-    {
-        result = ("working")
+    {   
+        ArrayFunction(StartNumber,EndNumber,StepNumber)
+      //  let ResultArray = (NumArray)
+      //  let ResultSum
+      //  let ResultBin
     }
-writeOutput(result)
+
+
+// writeOutput(result)
+
+
+// Adds or Subtracts based on StepNum and adds stuff into an array
+function ArrayFunction (ArrayStart,ArrayEnd,ArrayStep)
+{
+    if (ArrayStep>0)
+        {
+            for(let i = ArrayStart; i <= ArrayEnd; i = i + ArrayStep)
+                {
+                    NumArray.push(i)
+                }
+            return NumArray;
+        }
+    else
+        {
+            for(let i = ArrayStart; i >= ArrayEnd; i = i + ArrayStep)
+                {
+                    NumArray.push(i) 
+                }
+            return NumArray;
+        }
+}
 
 
 
-Number.isInteger(StartNumber);
-Number.isInteger(EndNumber);
-Number.isInteger(StepNumber);
+
+
+
+
+
+
+// Change to binary function
+function dec2bin(dec) {
+    return (dec >>>0).toString(2);
+    // the >>> rightshift to 0 to remove the sign
+    // make it able to handle negative number
+}
+
+
+
 
 function writeOutput(result){
     const h3Element = document.getElementById ('output')
